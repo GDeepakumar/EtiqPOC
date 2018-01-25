2580fc2ee98bf4e7b73dfd1e96f518c94324185c
GDeepakumar <gde@tiqri.com>
2018-01-25 17:48:59 +0530

    first sql statement

---
+++ DbInfoStudy.sql
@@ -0,0 +1,17 @@
+use TakstNett
+
+select top 10 * from [dbo].[BuDe_BuildingDescription]
+
+select top 10 * from [dbo].[Lev_LogEvents]
+
+select top 10 * from [dbo].[Lo_LogOns]
+
+select top 10 * from [dbo].[Ph_Phrase]
+
+select top 10 * from [dbo].[VaRev_Revision]
+
+select top 10 * from [dbo].[Person] where [E-postnavn] like '%gde%'
+
+select top 10 *  from [dbo].[Car_CalculationResource]
+
+select top 10 * from [dbo].[VAElg_ElementGroup]
