502bb734d1c9cd194a63bc456f8e077c26bb2476
GDeepakumar <gde@tiqri.com>
2018-02-06 14:13:19 +0530

    Package Usage Chart Created

--- DbInfoStudy.sql
+++ DbInfoStudy.sql
@@ -19,3 +19,12 @@ select top 10 * from [dbo].[VAElg_ElementGroup]
 select top 100  * from [dbo].[VaTa_Task] 
 
 select distinct Ta_TaskTypeName from [dbo].[VaTa_Task]
+
+select top 1000 * from [dbo].[PTO_TNLisenser]
+
+sp_help [PTO_TNLisenser]
+
+select * from [dbo].[PTO_TNLisenserStud]
+
+select top 100 * from [dbo].[PTO_TNLisenser]
+select top 100 * from [dbo].[VaLg_Log]
