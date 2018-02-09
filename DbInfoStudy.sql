9b8bededb8dc9a8fe66072ca22f02c2a5a6371ae
GDeepakumar <gde@tiqri.com>
2018-02-09 16:14:59 +0530

    AdminPanelV2 License drill down

--- DbInfoStudy.sql
+++ DbInfoStudy.sql
@@ -43,4 +43,42 @@ select top 100 * from [dbo].[VaTe_Term]
 select top 100 * from [dbo].[Pr_PriceBankLicense]
 
 select top 100 * from PTO_TNLisenser
-sp_help PTO_TNLisenser
+select top 100 * from Person
+select top 100 * from PersonIDMapping
+select top 100  * from Ad_Address
+select top 100 *  from AdCt_AddressContact
+select top 100 * from [dbo].[Co_Company]
+
+select top 100 * from Ad_Address
+
+sp_help Person
+EXEC sp_fkeys 'Person'
+
+SELECT fk.name AS Foreign_Key,
+SCHEMA_NAME(fk.schema_id) AS Schema_Name,
+OBJECT_NAME(fk.parent_object_id) AS Table_Name,
+SCHEMA_NAME(o.schema_id) Referenced_Schema_Name,
+OBJECT_NAME (fk.referenced_object_id) AS Referenced_Table_Name
+FROM sys.foreign_keys fk
+INNER JOIN sys.objects o ON fk.referenced_object_id = o.object_id
+ORDER BY Table_Name
+
+EXEC sp_fkeys 'Person'
+
+select top 100 * from Ad_Address
+select top 100 * from [dbo].[AdCt_AddressContact]
+select top 100 * from [dbo].[Co_Company]
+select top 100 * from [dbo].[UserProfile]
+select top 100 * from [dbo].[Users]
+select top 100 * from [dbo].[VaAdr_Address]
+
+select top 100 * from Person
+sp_help Person
+select top 100 * from PTD_FirmaDetalj
+select top 100 * from PTD_Firma
+
+select P.KundeId,P.PersonId,FD.FirmaId,F.FirmaNavn,F.FirmaAdr,F.FirmaPostNr,F.FirmaPostSted,F.FirmaKommune,F.FirmaFylke from Person P 
+Left Join PTD_FirmaDetalj FD On P.PersonId = FD.PersonID 
+Left Join PTD_Firma F on FD.FirmaID = F.FirmaID
+
+sp_help PTD_Firma
--- PowerBIView.sql
+++ PowerBIView.sql
@@ -5,5 +5,41 @@ go
 
 create view pbrep_VaTa_Task
 as
-select top 500 * from VaTa_Task where Ta_isDeleted = 0
+select top 500 *,'Country' as Country from VaTa_Task where Ta_isDeleted = 0
+go
+
+
+
+if exists(select 1 from sys.views where name='pbrep_Pto_InLisens' and type='v')
+drop view pbrep_VaTa_Task;
+go
+
+create view pbrep_Pto_InLisens
+as 
+select top 500 * from PTO_TNLisenser
+Go
+
+
+if exists(select 1 from sys.views where name='pbrep_Person' and type='v')
+drop view pbrep_Person;
+go
+
+create view pbrep_Person
+as 
+select top 500 * from Person
+Go
+
+if exists(select 1 from sys.views where name='pbrep_PersonCompany' and type='v')
+drop view pbrep_PersonCompany
+go
+
+create view pbrep_PersonCompany
+as 
+select P.KundeId,P.PersonId,FD.FirmaId,F.FirmaNavn,F.FirmaAdr,F.FirmaPostNr,F.FirmaPostSted,F.FirmaKommune,F.FirmaFylke from Person P 
+Left Join PTD_FirmaDetalj FD On P.PersonId = FD.PersonID 
+Left Join PTD_Firma F on FD.FirmaID = F.FirmaID
+go
+
+
+
 
